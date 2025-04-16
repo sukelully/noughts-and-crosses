b932216058731077277992ab9c8605ead5328932
Luke Sullivan <sully097@gmail.com>
2025-04-16 16:54:37 +0100

    Save

---
+++ script.js
@@ -0,0 +1,13 @@
+const Gameboard = (function() {
+    const board = [];
+
+    return { board };
+})();
+
+const GameController = (function() {
+    
+});
+
+const createPlayer = ()
+
+console.log(Gameboard.gameArray);
